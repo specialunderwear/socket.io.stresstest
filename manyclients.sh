a473a9591402646acef6689635a321457ffa0fda
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-01-24 14:42:01 +0000

    better

--- manyclients.sh
+++ manyclients.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 echo "Spawning $1 processes"
+FAIL=0
 for (( i = 0; i <= $1 ; i++ ))
 do
-    ./websocketclient localhost:81 input.json &
+    ./websocketclient localhost:81 input.json > /dev/null &
 done
+for job in `jobs -p`
+do
+    wait $job || let "FAIL+=1"
+done
+
+if [ "$FAIL" == "0" ];
+then
+echo "All clients completed succesfully."
+else
+echo "O noes ($FAIL) clients crashed!"
+fi
