df6c8ca7eb8420e200b49ab01b78a1ddc1e66c94
Lars van de Kerkhof <lars@permanentmarkers.nl>
2012-02-01 10:11:30 +0000

    improved manyclients

--- manyclients.sh
+++ manyclients.sh
@@ -2,16 +2,25 @@
 echo "Spawning $1 processes"
 T="$(date +%s)"
 FAIL=0
+pidlist=""
 for (( i = 0; i <= $1 ; i++ ))
 do
     if [ $4 ]; then
         ./websocketclient $2 $3 &
+	pidlist="$pidlist $!"
+	sleep 0.001s
     else
         ./websocketclient $2 $3 > /dev/null &
+	pidlist="$pidlist $!"
+	sleep 0.001s
     fi
 done
-for job in `jobs -p`
+echo "Done spawning processes in $(($(date +%s)-T)) seconds"
+echo "Waiting for completion now ..."
+WAIT=0
+for job in $pidlist
 do
+    let WAIT+=1
     wait $job || let "FAIL+=1"
 done
 
